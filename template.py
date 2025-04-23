4a499e10556a137a45537bb7271a2130f655dc09
anshu1016 <arunshukla98710@gmail.com>
2025-04-23 11:17:49 +0530

    folder structure updated

--- template.py
+++ template.py
@@ -3,32 +3,33 @@ import os
 from pathlib import Path
 
 list_of_files = [
-    'github/workflows/.gitkeep',
-    'src/__init__.py',
-    'src/components/__init__.py',
-    'src/components/data_ingestion.py',
-    'src/components/data_transformation.py',
-    'src/components/model_trainer.py',
-    'src/components/model_evaluation.py',
-    'src/pipeline/__init__.py',
-    'src/pipeline/training_pipeline.py',
-    'src/pipeline/pipeline_prediction.py',
-    'src/utils/__init__.py',
-    'src/utils/utils.py',
-    'src/logger/logging.py',
-    'src/exception/exception.py'
-    'tests/units/__init__.py',
-    'tests/integration/__init__.py',
-    'init_setup.sh',
-    'requirements.txt',
-    'requirements_dev.txt',
-    'setup.py',
-    'setup.config',
-    'pyproject.toml',
-    'experiment/experiments.ipynb',
-    'README.md',
-    
+".github/workflows/.gitkeep",
+"src/init.py",
+"src/components/init.py",
+"src/components/data_ingestion.py",
+"src/components/data_transformation.py",
+"src/components/model_trainer.py",
+"src/components/model_evaluation.py",
+"src/pipeline/init.py",
+"src/pipeline/training_pipeline.py",
+"src/pipeline/prediction_pipeline.py",
+"src/utils/init.py",
+"src/utils/utils.py",
+"src/logger/logging.py",
+"src/exception/exception.py",
+"tests/unit/init.py",
+"tests/integration/init.py",
+"init_setup.sh",
+"requirements.txt",
+"requirements_dev.txt",
+"setup.py",
+"setup.cfg",
+"pyproject.toml",
+"tox.ini",
+"experiment/experiments.ipynb",
 ]
+
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
